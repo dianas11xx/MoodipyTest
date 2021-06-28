46414eb79fb783af7c1b22d2fbb1559b0c2e1a5a
dianas11xx <40523965+dianas11xx@users.noreply.github.com>
2021-06-28 13:10:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup
 
+
 setup(
     name="Moodipy",
-    version="0.1",
-    packages=["Moodipy"],
+    version="0.103",
+    packages=['Moodipy',],
     url="https://github.com/dianas11xx/MoodipyTest",
     author="Noteworthy",
     license = "GPL 3",
-    package_data = {"Moodipy":['imgs/*jpeg', 'imgs/*png']},
+    package_data = {"Moodipy":['imgs/awful.jpeg', 'imgs/arrow.png', 'emotions.txt']},
     description="use sentiment analysis to create a playlist that matches someone's mood and also predict songs to rise in popularity.",
-    install_requires=["python3.8", "spotipy==2.18.0", "nltk==3.6.2", "PyQt5==5.12", "PyQt5-sip==4.19.19", "PyQt5.QtWidgets", "PyQt5.QtGui", "PyQt5.QtCore", "screeninfo", "requests.exceptions", "sys", "os", "nltk.downloader punkt", "nltk.downloader stopwords", "nltk.downloader averaged_perceptron_tagger", "nltk.downloader wordnet", "pyqt5-dev-tools", "qttools5-dev-tools"],
-    entry_points={"console_scripts": ["start_Moodipy=GUI:main"]}
+    install_requires=['nltk==3.6.2', 'screeninfo==0.6.7', 'spotipy==2.18.0', 'requests>=2.25.0', 'PyQt5>=5.12','urllib3>=1.26.0', 'PyQt5-sip>=4.19.19'],
+    entry_points={"console_scripts": ["start_Moodipy=Moodipy.main:main"]},
+    include_package_data=True
 )
+
